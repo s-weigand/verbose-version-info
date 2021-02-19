41f7e2ed0496a1c9194e18a4ce3d6aeaba2467d9
s-weigand <s.weigand.phy@gmail.com>
2021-02-19 16:44:33 +0100

    Added compat module for python < 3.8

--- verbose_version_info/metadata_compat.py
+++ verbose_version_info/metadata_compat.py
@@ -1,5 +1,10 @@
 """Compatibility module for importlib.metadata in python < 3.8 ."""
+
 try:
+    from importlib.metadata import Distribution
+    from importlib.metadata import PackageNotFoundError
     from importlib.metadata import distribution
 except ImportError:
+    from importlib_metadata import Distribution  # type: ignore
+    from importlib_metadata import PackageNotFoundError  # type: ignore
     from importlib_metadata import distribution  # type: ignore
