d35643dddebd279d164bc0a151b9f2dd63998e58
s-weigand <s.weigand.phy@gmail.com>
2021-02-19 16:54:55 +0100

    Implemented 'get_distribution' with 'NotFoundDistribution' dummy class
    This is in order not to repeat the try-except pattern over and over again.
    Also this allows a simple use of central settings for not found packages so the output will be consisten.

--- tests/test_verbose_version_info.py
+++ tests/test_verbose_version_info.py
@@ -1,13 +1,59 @@
-#!/usr/bin/env python
+"""Tests for ``verbose_version_info`` package."""
 
-"""Tests for `verbose_version_info` package."""
+from pathlib import Path
 
-# import pytest
+import pytest
 
+from verbose_version_info import SETTINGS
 from verbose_version_info import __version__
+from verbose_version_info.metadata_compat import distribution
+from verbose_version_info.verbose_version_info import NotFoundDistribution
 from verbose_version_info.verbose_version_info import basic_version
+from verbose_version_info.verbose_version_info import get_distribution
 
 
-def test_basic_version():
-    """Sample pytest test function with the pytest fixture as an argument."""
-    assert basic_version("verbose-version-info") == __version__
+def test_get_distribution():
+    """Valid distribution."""
+    result = get_distribution("verbose-version-info")
+    expected = distribution("verbose-version-info")
+
+    assert result.version == expected.version
+    assert result.files == expected.files
+    assert result.requires == expected.requires
+
+
+def test_get_distribution_not_found():
+    """Invalid distribution."""
+    result = get_distribution("not-a-distribution")
+
+    assert isinstance(result, NotFoundDistribution)
+    assert result.version == "Unknown"
+    assert result.files == []
+    assert result.requires == []
+
+    # Just for coverage of NotFoundDistribution
+    assert result.read_text("foo") is None
+    assert result.locate_file("foo") == Path("foo")
+    assert result.locate_file(Path("foo")) == Path("foo")
+
+
+@pytest.mark.parametrize(
+    "distribution_name,expected",
+    (
+        ("verbose-version-info", __version__),
+        ("verbose_version_info", __version__),
+        ("git-install-test-distribution", "0.0.2"),
+        ("git_install_test_distribution", "0.0.2"),
+        ("not-a-distribution", "Unknown"),
+    ),
+)
+def test_basic_version(distribution_name: str, expected: str):
+    """Expected default behavior with default settings."""
+    assert basic_version(distribution_name) == expected
+
+
+def test_changing_not_found_version_str(monkeypatch):
+    """Changed setting 'not_found_version_str' changes version string."""
+    monkeypatch.setitem(SETTINGS, "not_found_version_str", "Not Installed")
+
+    assert basic_version("not-a-distribution") == "Not Installed"
--- verbose_version_info/verbose_version_info.py
+++ verbose_version_info/verbose_version_info.py
@@ -1,18 +1,146 @@
 """Main module."""
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from verbose_version_info import SETTINGS
+from verbose_version_info.metadata_compat import Distribution
+from verbose_version_info.metadata_compat import PackageNotFoundError
 from verbose_version_info.metadata_compat import distribution
 
+if TYPE_CHECKING:
+    from os import PathLike
 
-def basic_version(distribution_name: str) -> str:
+
+class NotFoundDistribution(Distribution):
+    """Distribution of package which couldn't be found.
+
+    This class is used not to repeat the try-except pattern over and
+    over again.
+    If a package isn't found an instance of this class is returned by
+    :func:`get_distribution` and the parts of the API which are used by
+    ``verbose_version_info`` are kept intact for the way they are used.
+
+    See Also
+    --------
+    get_distribution
+    """
+
+    def read_text(self, filename: str) -> None:
+        """Attempt to load metadata file given by the name.
+
+        Just added to satisfy mypy, since the superclass one is
+        an ``abstractmethod``.
+
+        Parameters
+        ----------
+        filename : str
+            Name of a file in the distribution
+
+        Returns
+        -------
+        None
+        """
+        return None
+
+    def locate_file(self, path: PathLike | str) -> PathLike:
+        """Given a path to a file in this distribution, return a path to it.
+
+        Just added to satisfy mypy, since the superclass one is
+        an ``abstractmethod``.
+
+        Parameters
+        ----------
+        path: PathLike | str
+            Path to a file.
+
+        Returns
+        -------
+        PathLike
+            Just the initial ``path`` ensure to be type ``PathLike``.
+        """
+        if isinstance(path, str):
+            return Path(path)
+        else:
+            return path
+
+    @property
+    def version(self) -> str:
+        """User set string if no version was found.
+
+        Default "Unknown".
+
+        Returns
+        -------
+        str
+            SETTINGS["not_found_version_str"]
+        """
+        return SETTINGS["not_found_version_str"]
+
+    @property
+    def files(self) -> list:
+        """List of files in this distribution.
+
+        Not having found a distribution is (in this usecase)
+        equivalent to it not having any files.
+
+        Returns
+        -------
+        list
+            Empty list
+        """
+        return []
+
+    @property
+    def requires(self) -> list:
+        """List of generated requirements specified for this Distribution.
+
+        Not having found a distribution is (in this usecase)
+        equivalent to it not having any requirements.
+
+        Returns
+        -------
+        list
+            Empty list
+        """
+        return []
+
+
+def get_distribution(
+    distribution_name: str,
+) -> Distribution:
+    """Get the ``Distribution`` instance for the named package.
+
+    Parameters
+    ----------
+    distribution_name : str
+        The name of the package as a string.
+
+    Returns
+    -------
+    Distribution
+        Distribution instance of the package
+    """
+    try:
+        return distribution(distribution_name)
+    except PackageNotFoundError:
+        return NotFoundDistribution()
+
+
+def basic_version(
+    distribution_name: str,
+) -> str:
     """Retrieve the basic version of a distribution.
 
     Parameters
     ----------
     distribution_name : str
-        [description]
+        The name of the distribution package as a string.
 
     Returns
     -------
     str
         Version string of the distribution
     """
-    return distribution(distribution_name).version
+    return get_distribution(distribution_name).version
