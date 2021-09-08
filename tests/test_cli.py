c4d6fd876321e693c9306f6f10c380671e7265de
s-weigand <s.weigand.phy@gmail.com>
2021-09-08 21:31:06 +0200

    👌 Changed CLI framework from click to typer and rich

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,17 +1,18 @@
 """Tests for the CLI"""
+import re
 import sys
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
-from click.testing import CliRunner
+from typer.testing import CliRunner
 
-from verbose_version_info import cli
+from verbose_version_info.cli import cli
 
 
 def test_missing_cli_extra_requires(monkeypatch: MonkeyPatch):
     """Exception raised if cli extra_requires is missing"""
     monkeypatch.delitem(sys.modules, "verbose_version_info.cli")
-    monkeypatch.setitem(sys.modules, "click", None)
+    monkeypatch.setitem(sys.modules, "typer", None)
 
     with pytest.raises(ImportError, match=r"pip install verbose-version-info\[cli\]"):
         import verbose_version_info.cli  # noqa: F401
@@ -20,9 +21,9 @@ def test_missing_cli_extra_requires(monkeypatch: MonkeyPatch):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli)
     assert result.exit_code == 0
     assert "Not yet Implemented!" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
+    help_result = runner.invoke(cli, ["--help"])
     assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+    assert re.search(r"--help\s+Show this message and exit\.", help_result.output) is not None
--- verbose_version_info/cli.py
+++ verbose_version_info/cli.py
@@ -1,8 +1,7 @@
 """Console script for verbose_version_info."""
-import sys
 
 try:
-    import click
+    import typer
 except ImportError:
     raise ImportError(
         "The requirements for the cli usage aren't installed.\n"
@@ -10,8 +9,10 @@ except ImportError:
         "`pip install verbose-version-info[cli]`"
     )
 
+cli = typer.Typer(name="vvinfo")
 
-@click.command()
+
+@cli.command()
 def main(args=None) -> int:
     """Console script for verbose_version_info.
 
@@ -25,9 +26,9 @@ def main(args=None) -> int:
     int
         Returncode
     """
-    click.echo("Not yet Implemented!")
+    print("Not yet Implemented!")
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    cli()
